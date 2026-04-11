3fdc71d6e7441ea85f49ddb91287acc9613e422e
rejuanigg <riosjuan32223@gmail.com>
2026-04-11 11:25:38 -0300

    feat(api): implement api in category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\SectionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,4 +17,5 @@ Route::middleware('auth:sanctum')->group(
     {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::apiResource('sections', SectionController::class)->names('api.');
+        Route::apiResource('categories', CategoryController::class)->names('api.');
     });
